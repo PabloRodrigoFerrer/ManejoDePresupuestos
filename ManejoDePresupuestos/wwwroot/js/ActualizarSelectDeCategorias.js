46cba09d59362286ecf64e75434a32da76c9cc1d
PabloRodrigoFerrer <rferrer42@gmail.com>
2026-02-08 19:38:16 -0300

    Centralizamos en un script una función para actualizar el select list de categoria dependiendo de si son gastos o ingresos

---
+++ ManejoDePresupuestos/wwwroot/js/ActualizarSelectDeCategorias.js
@@ -0,0 +1,27 @@
+﻿
+
+function ActualizarSelectCategorias(urlObtenerCategorias) {
+
+    $('#TipoOperacionId').change(async function () {
+        //seleccionar el valor
+        const valorSeleccionado = $(this).val();
+
+        const respuesta = await fetch(
+            urlObtenerCategorias,
+            {
+                method: 'POST',
+                body: valorSeleccionado,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+        const json = await respuesta.json();
+        console.log(json);
+
+        const opciones =
+            json.map(categoria => `<option value=${categoria.value}>${categoria.text}</option>`);
+
+        $('#CategoriaId').html(opciones);
+    })
+}
