e3bea1d993406adc184a739ea79f8a915bd2439c
PabloRodrigoFerrer <rferrer42@gmail.com>
2026-02-11 19:24:34 -0300

    Ahora navegaciónFechas es un viewmodel para la vista parcial de navegación que sabe calcular meses anteriores y posteriores al igual que años a través de una fecha de referencia, esto libera mucha logica de otros viewmodels dejando el codigo más limpio y reutilizable.

---
+++ ManejoDePresupuestos/ViewModelPartials/NavegacionFechasViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ManejoDePresupuestos.ViewModelPartials
+{
+    public class NavegacionFechasViewModel
+    {
+        public DateTime FechaReferencia { get; set; }
+        public int MesAnterior => FechaReferencia.AddMonths(-1).Month;
+        public int AñoAnterior => FechaReferencia.AddMonths(-1).Year;
+        public int MesPosterior => FechaReferencia.AddMonths(1).Month;
+        public int AñoPosterior => FechaReferencia.AddMonths(1).Year;
+        public string Accion {  get; set; } = string.Empty;
+        public int? Parametro { get; set; }
+    }
+}
