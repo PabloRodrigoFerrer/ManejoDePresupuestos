0be906b7b9fcf32c73289005a1d977c1180de163
PabloRodrigoFerrer <rferrer42@gmail.com>
2026-02-11 19:19:55 -0300

    Creación de vista parcial para centralizar los totales generales con su viewmodel

---
+++ ManejoDePresupuestos/ViewModelPartials/BalanceTotales.cs
@@ -0,0 +1,9 @@
+﻿namespace ManejoDePresupuestos.ViewModelPartials
+{
+    public class BalanceTotalesViewModel
+    {
+        public decimal Ingresos { get;set;}
+        public decimal Gastos { get;set; }
+        public decimal Total { get;set; }
+    }
+}
