b63ee47bbeff7145e410e1c44da3540691ad02bc
PabloRodrigoFerrer <rferrer42@gmail.com>
2026-02-12 18:15:24 -0300

    refactorización de clase y partialView para navegar, ahora NavegacionPorMeses, y actualización en las vistas que la utilizan

---
+++ ManejoDePresupuestos/ViewModelPartials/NavegacionPorAñosViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ManejoDePresupuestos.ViewModelPartials
+{
+    public class NavegacionPorAñosViewModel
+    {
+        public DateTime FechaReferencia { get; set; }
+        public int AñoPosterior => FechaReferencia.AddYears(1).Year;
+        public int AñoAnterior => FechaReferencia.AddYears(-1).Year;
+        public string Accion { get; set; } = string.Empty;
+    }
+}
